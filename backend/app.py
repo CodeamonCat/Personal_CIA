c5d7ab77848c610864d4dc8560c80f466824051a
kautoku <b06705022@ntu.edu.tw>
2023-05-15 05:44:49 +0800

    connect summary api, update mission api

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,14 @@ import openai
 
 # company_name=""
 # industry=""
-openai.api_key=""
+### config ###################################
+debug_mode = False
+AUTO_GPT_PATH = '/home/bacon/Auto-GPT'
+##############################################
+
+with open("backend/openai.api_key","r") as f:
+    api_key = f.read()
+    openai.api_key=api_key
 agent_dates=[datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")]
 agent_prompt=['Collect and analyze data for a single investment target or an expanded target set by adding one more target.']
 agent_step=[10]
@@ -71,26 +78,54 @@ def new_mission():
         agent_prompt.append(prompt)
         agent_step.append(attempt)
         agent_status.append(0)
-
-        # file_name=time_string+"_done.txt"
-        # in_file_name=time_string+"_in.txt"
-        # out_file_name=time_string+"_out.txt"
-        # with open("in_file_name", "w") as file:
-        #     file.write("n\ngeneral ai\nresearcher\n")
-        #     file.write(prompt+" and output it to "+file_name+"\n\ny -"+attempt)
-        # os.chdir(os.path.dirname(""))
-        # command = "nohup ./home/bacon/Auto-GPT/run.sh < /home/bacon/Auto-GPT/test_io/"+in_file_name+" > /home/bacon/Auto-GPT/test_io/"+out_file_name
-        # result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-
+        agent_number_tmp = agent_number
         agent_number += 1 
+
+        if debug_mode == False:
+            file_name=time_string+"_done.txt"
+            in_file_name=time_string+"_in.txt"
+            out_file_name=time_string+"_out.txt"
+            # get current dir
+            cwd = os.getcwd() 
+
+            # change dir to Auto-GPT
+            os.chdir(AUTO_GPT_PATH) 
+            print(os.getcwd())
+            with open("./test_io/"+in_file_name, "w") as file:
+                file.write("n\ngeneral ai\nresearcher\n")
+                file.write(prompt+" and output it to "+file_name+"\n\ny -"+str(attempt))
+            command = "nohup ./run.sh < ./test_io/"+in_file_name+" > ./test_io/"+out_file_name
+            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+            
+            # check if process is done or error
+            file_path = "./auto_gpt_workspace/"+agent_dates[agent_number_tmp]+"_done.txt"
+            if os.path.isfile(file_path):
+                agent_status[agent_number_tmp] = 1
+            else:
+                agent_status[agent_number_tmp] = -1
+
+            # return to project dir
+            os.chdir(cwd)
+
+            
+
         return jsonify({'name':prompt,'date':time_string,'status':0}), 200
     return 'Error', 400
 
 @app.route('/api/missions', methods=['GET'])
 def missions():
+    for i in range(agent_number):
+        file_path = "/home/bacon/Auto-GPT/auto_gpt_workspace/"+agent_dates[i]+"_done.txt"
+        if os.path.isfile(file_path):
+            agent_status[i] = 1
+        else:
+            agent_status[i] = -1
+    
     mission_list = [{
+                     'id' : i,
+                     'selected': False,
                      'name': agent_prompt[i],
-                     'status': ('Completed' if agent_status[i] else 'Processing'),
+                     'status': ('Completed' if agent_status[i] == 1 else 'Failed' if agent_status[i] == -1  else 'Processing'),
                      'createdAt': agent_dates[i]
                      } for i in range(agent_number)]
     print(mission_list)
@@ -112,7 +147,7 @@ def delete_mission():
     # {
     #   index: int
     # }
-    index = request.form.get('index')
+    index = request.json.get('index')
     # delete agent and files
     file_path = "/home/bacon/Auto-GPT/auto_gpt_workspace/"+agent_dates[index]+"_done.txt"
     os.remove(file_path)
@@ -130,18 +165,34 @@ def summary():
     # {
     #   indeces: list of integer
     # }
-    indices = request.form.get('indices')
+    indices = request.json.get('indices')
     #
-    data_to_sent=""
-    for i in len(indices):
-        file_path = "/home/bacon/Auto-GPT/auto_gpt_workspace/"+agent_dates[indices[i]]+"_done.txt"
-        with open(file_path, "r") as file:
-            data_to_sent+= file.read()
-    prompt="please summarize the following passage:"+data_to_sent
-    response = openai.Completion.create(engine=model,prompt=prompt,temperature=temperature,max_tokens=max_tokens)
+    if len(indices) <= 0:
+        return jsonify({'error': "no indices is selected"}), 400
+
+    if debug_mode == False:
+        # get current dir
+        cwd = os.getcwd() 
+
+        # change dir to Auto-GPT
+        os.chdir(AUTO_GPT_PATH) 
+
+        data_to_sent=""
+        for i in range(len(indices)):
+            file_path = "./auto_gpt_workspace/"+agent_dates[indices[i]]+"_done.txt"
+            with open(file_path, "r") as file:
+                data_to_sent+= file.read()
+        prompt="please summarize the following passage:"+data_to_sent
+        response = openai.Completion.create(engine=model,prompt=prompt,temperature=temperature,max_tokens=max_tokens)
+
+        # return to project dir
+        os.chdir(cwd)
 
     # output: long string
-    return jsonify({'summary': response.choices[0].text}), 200
+    if debug_mode == False:
+        return jsonify({'summary': response.choices[0].text}), 200
+    else:
+        return jsonify({'summary': "alert this"}), 200
 
 if __name__ == "__main__":
 	app.run(debug=True)
